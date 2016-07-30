36e35de1f457642e43c44acfe56463dfd1ee6f45
zpzeng <zhipengzeng@126.com>
2016-07-30 19:19:04 +0800

    println("Hello World")

---
+++ src/com/zpzeng/test/TestClass.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)TestClass.java
+ *
+ * Copyright (c) 2015-2016 Knight
+ * 2999 North ReMin Road, SongJiang, ShangHai, China.
+ * All Rights Reserved.
+ *
+ * The software's copyright statement...
+ */
+
+package com.zpzeng.test;
+
+/**
+ *<code>TestClass.java</code>
+ *
+ *@version 1.0 
+ *@author knight
+ */
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		System.out.println("hello World");
+	}
+
+}
