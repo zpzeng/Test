fa2f8565bdeed84cd7db39db4c6ecb2873982d1c
zpzeng <zhipengzeng@126.com>
2016-07-30 19:54:11 +0800

    新增一个类

---
+++ src/com/zpzeng/test/Test2.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)Test2.java
+ *
+ * Copyright (c) 2015-2016 Knight
+ * 2999 North ReMin Road, SongJiang, ShangHai, China.
+ * All Rights Reserved.
+ *
+ * The software's copyright statement...
+ */
+
+package com.zpzeng.test;
+
+/**
+ *<code>Test2.java</code>
+ *
+ *@version 1.0 
+ *@author knight
+ */
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		System.out.println("¡Ì“ª∏ˆ¿‡");
+	}
+
+}
