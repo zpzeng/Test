170e951a78450f02532de0c2af6a4ee1922e9829
zpzeng <zhipengzeng@126.com>
2016-08-02 11:18:12 +0800

    测试提交

---
+++ src/com/zpzeng/controller/MyController.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)MyController.java
+ *
+ * Copyright (c) 2015-2016 Knight
+ * 2999 North ReMin Road, SongJiang, ShangHai, China.
+ * All Rights Reserved.
+ *
+ * The software's copyright statement...
+ */
+
+package com.zpzeng.controller;
+
+/**
+ *<code>MyController.java</code>
+ *
+ *@version 1.0 
+ *@author knight
+ */
+
+public class MyController {
+
+}
---
+++ src/com/zpzeng/service/MyService.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)MyService.java
+ *
+ * Copyright (c) 2015-2016 Knight
+ * 2999 North ReMin Road, SongJiang, ShangHai, China.
+ * All Rights Reserved.
+ *
+ * The software's copyright statement...
+ */
+
+package com.zpzeng.service;
+
+/**
+ *<code>MyService.java</code>
+ *
+ *@version 1.0 
+ *@author knight
+ */
+
+public class MyService {
+
+}
